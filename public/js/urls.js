c2e9b1511e768dfca347735f8fe97118049e22b6
Jungsik <sk7556@naver.com>
2023-12-26 14:00:23 +0900

    ✅ main 파일과 동기화

---
+++ public/js/urls.js
@@ -0,0 +1,14 @@
+const url = "https://www.infinity-travel.shop"
+const mydataurl = url + "/accounts/"
+const signupurl = url + '/accounts/create/'
+const loginurl = url + '/accounts/login/'
+const roomlisturl = url + "/chat/roomlist/"
+const roomdeleteurl = url + "/chat/roominvisible/"
+const chathistoryurl = url + "/chat/"
+const chatws =  "ws://127.0.0.1:8000/ws/chat/"
+
+
+
+const page = "https://www.infinity-travel.shop/front/pages"
+const loginpage = page + "/login.html"
+const mainpage = page + "/index.html"
